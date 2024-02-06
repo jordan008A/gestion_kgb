8a1073232fe5783865a7136b873a9af5030e3a06
jordan008A <jordancros@hotmail.fr>
2024-02-06 13:59:13 +0100

    💄 Add templates and CSS style

--- controllers/missionsController.php
+++ controllers/missionsController.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../config/database.php';
+require_once '../public/config/database.php';
 
 $missions = [];
 
---
+++ views/partials/_headerView.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion des Missions</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../public/css/style.css">
+</head>
+<body>
+<header class="bg-dark text-white py-3 text-center">
+    <h1>Gestion des Missions KGB</h1>
+</header>
